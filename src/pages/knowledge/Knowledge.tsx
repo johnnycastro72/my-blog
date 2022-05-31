ab01d66b2f5fbbedfc92953d5283b14129108160
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-31 17:18:14 -0500

    Knowledge and portfolio added

---
+++ src/pages/knowledge/Knowledge.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../../App.css'
+
+const Knowledge = () => {
+  return (
+    <div className='main_content'>
+      <h1>Knowledge</h1>
+      <ul>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>Git</li>
+        <li>MySQL</li>
+        <li>MongoDB</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Knowledge
---
+++ src/pages/portfolio/Portfolio.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='main_content'>
+      <h1>Portfolio</h1>
+      <ul>
+          <li>
+              <a href="https://github.com/johnnycastro72">GitHub</a>
+          </li>
+          <li>
+              <a href="https://www.linkedin.com/in/jhonnycastroclavijo/">LinkedIn</a>
+          </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Portfolio
